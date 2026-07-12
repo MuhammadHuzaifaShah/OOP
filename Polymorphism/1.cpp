de7a415db7d1adb1c1bcba15ddc9423ea3887c3d
Muhammad Huzaifa Shah <shahhu823@gmail.com>
2026-07-12 22:46:48 +0500

    I added some New files

---
+++ Polymorphism/1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+polymorphism 
+==>One name and Multple Forms
+
+Polymorphism in C++ has two types:
+1.Compile time PolyMorphism
+Compile time PolyMorphism is achieved by:
+-->i.Function Overloading
+-->ii.Operator overloading
+2.Run time polyMorphism
+Run time PolyMorphism is achieved by:
+i.Virtual Function
+*/
+int main()
+{
+
+   return 0;
+}
---
+++ virtual Function/1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class BaseClass{
+    public:
+    int var_base;
+    virtual void display(){
+        cout << "Displaying the base Variable " << var_base << "\n";
+    }
+};
+class DerivedClass:public BaseClass{
+    public:
+    int var_Derived=2;
+    void display(){
+        cout << "Displaying the DErived Variable " << var_Derived << "\n";
+        cout << "Displaying the base Variable from derived class " << var_base << "\n";
+    }
+};
+int main()
+{
+    BaseClass *base_Class_Pointer;
+    BaseClass obj_Base;
+    DerivedClass obj_Derived;
+    base_Class_Pointer= &obj_Derived; 
+    base_Class_Pointer->var_base=34;
+    // base_Class_Pointer->var_derived=134; This will throw an error
+    base_Class_Pointer->display();
+
+    // DerivedClass *derived_Class_pointer;
+    // derived_Class_pointer=&obj_Derived;
+    // derived_Class_pointer->var_Derived=320;
+    // derived_Class_pointer->display();
+  
+   return 0;
+}
