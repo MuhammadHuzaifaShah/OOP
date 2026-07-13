db82f1bec633395abef915fa597c365c2c49c212
Muhammad Huzaifa Shah <shahhu823@gmail.com>
2026-07-13 14:46:05 +0500

    Virtual Funtion is added

---
+++ virtual Function/2.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+class CWH{
+    protected:
+        string title;
+        float rating;
+    public:
+        CWH(string s,float r){
+            title=s;
+            rating=r;
+        }
+        virtual void display(){ // if we donot write virtual it will gives back material in this dislay
+            cout << "Hi \n";
+        }  
+};
+
+class CWHVIDEO:public CWH{
+    float videoLength;
+    public:
+        CWHVIDEO(string s,float r,float vl):CWH(s,r){
+            videoLength=vl;
+        }
+    void display(){
+        cout << "This is amazing video with title " << title  << "\n";
+        cout << "rating " << rating  << " out of 5\n";
+        cout << "The length of this video is  " << videoLength  << " minutes.\n";
+    }
+};
+
+class CWHTEXT:public CWH{
+    int word;
+    public:
+        CWHTEXT(string s,float r,int wc):CWH(s,r){
+            word=wc;
+        }
+    void display(){
+        cout << "This is amazing video with title " << title  << "\n";
+        cout << "rating " << rating  << " out of 5\n";
+        cout << "The size of this video is  " << word  << " words.\n";
+    }
+};
+
+int main()
+{
+    string title;
+    float rating,vlen;
+    int words;
+
+    title="Djanjo Video";
+    rating=4.8;
+    vlen=6.7;
+    // for Video
+    CWHVIDEO djVideo(title,rating,vlen);
+    // djVideo.display();
+
+    // for text
+    
+    title="Djanjo Video";
+    rating=4.8;
+    words=150;
+    CWHTEXT djText(title,rating,words);
+    // djText.display();
+
+    CWH* tuts[2];
+    tuts[0]=&djVideo;
+    tuts[1]=&djText;
+    tuts[0]->display();
+    tuts[1]->display();
+
+
+   return 0;
+
+}
